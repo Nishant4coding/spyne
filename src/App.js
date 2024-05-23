4649584e51b9ff363d0fe8a785dcd1fc1ea53020
Nishant4coding <srivastava4nishant@gmail.com>
2024-05-23 23:39:17 +0530

    path name change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/main" element={<Main />} />
-          <Route path="/documentation" element={<Doc />} />
+          <Route path="/doc" element={<Doc />} />
         </Routes>
         <Footer/>
       </Router>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,13 +11,13 @@ function Footer() {
     <div className="bottom-0 text-center bg-orange-500 w-full lg:text-3xl text-xl lg:p-2 p-1 fixed ">
     <div className="flex flex-row items-center justify-center lg:gap-5 gap-2 ">
       
-      <Link to="https://github.com/Nishant4coding/" className="font-bold">
+      <Link to="https://www.linkedin.com/in/nishant-srivastava-/" className="font-bold">
         <RiLinkedinBoxFill />
       </Link>
       <Link to="https://github.com/Nishant4coding/" className="font-bold">
         <RiGithubFill />
       </Link>
-      <Link to="https://github.com/Nishant4coding/" className="font-bold">
+      <Link to="https://www.instagram.com/srivastava4nishant" className="font-bold">
         <RiInstagramFill />
       </Link>
       <Link
